22d2887c6813a55bb35a56acc9c87b2debf67ad4
Mathijs Verhaegh <mathijs@verhaegh.nl>
2025-10-24 22:28:20 +0200

    Util repr

--- src/solidbox/util.py
+++ src/solidbox/util.py
@@ -1,10 +1,11 @@
+from abc import ABCMeta, abstractmethod
+from attrs import define, field
 from solid2.core.object_base import BareOpenSCADObject
 from .main import Bbox
-from attrs import define, field
 
 
-@define
-class _Util:
+@define(repr=False)
+class _Util(metaclass=ABCMeta):
     _scad_object: BareOpenSCADObject
     _quiet: bool = True
     bbox: Bbox = field(init=False)
@@ -12,62 +13,81 @@ class _Util:
     def __attrs_post_init__(self):
         self.bbox = Bbox.from_scad(self._scad_object, quiet=self._quiet)
 
+    @property
+    @abstractmethod
+    def x(self) -> float:
+        ...
 
-@define
+    @property
+    @abstractmethod
+    def y(self) -> float:
+        ...
+
+    @property
+    @abstractmethod
+    def z(self) -> float:
+        ...
+
+    def __repr__(self, attrs=("x", "y", "z")):
+        dims = [f"{p} = {getattr(self, p)}" for p in attrs]
+        return type(self).__name__ + "\n  ".join(("", *dims))
+
+
+@define(repr=False)
 class Size(_Util):
     @property
-    def x(self) -> float:
+    def x(self):
         return self.bbox.depth
 
     @property
-    def y(self) -> float:
+    def y(self):
         return self.bbox.width
 
     @property
-    def z(self) -> float:
+    def z(self):
         return self.bbox.height
 
 
-@define
+@define(repr=False)
 class Mid(_Util):
     @property
-    def x(self) -> float:
+    def x(self):
         return self.bbox.center.x
 
     @property
-    def y(self) -> float:
+    def y(self):
         return self.bbox.center.y
 
     @property
-    def z(self) -> float:
+    def z(self):
         return self.bbox.center.z
 
 
-@define
+@define(repr=False)
 class Max(_Util):
     @property
-    def x(self) -> float:
+    def x(self):
         return self.bbox.x_max
 
     @property
-    def y(self) -> float:
+    def y(self):
         return self.bbox.y_max
 
     @property
-    def z(self) -> float:
+    def z(self):
         return self.bbox.z_max
 
 
-@define
+@define(repr=False)
 class Min(_Util):
     @property
-    def x(self) -> float:
+    def x(self):
         return self.bbox.x_min
 
     @property
-    def y(self) -> float:
+    def y(self):
         return self.bbox.y_min
 
     @property
-    def z(self) -> float:
+    def z(self):
         return self.bbox.z_min
